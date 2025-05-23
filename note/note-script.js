aabecfdb9dee324a72f3ade14de783971737857f
mimmizzang <fpahs0920@gamil.com>
2025-05-23 19:22:56 +0900

    add AOS animation to main page and update nav layout on note page

--- note/note-script.js
+++ note/note-script.js
@@ -1,13 +1,11 @@
 // header nav 슬라이드 기능
 const navSwiper = new Swiper(".nav-swiper", {
   slidesPerView: "auto",
-  spaceBetween: 20,
-  freeMode: true,
-  grabCursor: true,
   loop: true,
+  spaceBetween: 10,
   autoplay: {
     delay: 2000,
-    disableOnInteraction: false,
+    disableOnInteraction: true,
   },
 });
 
